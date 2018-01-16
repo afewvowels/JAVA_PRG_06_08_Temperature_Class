53a349d9b6883575bffadd7e0bbf861292e6d510
aeiou <aeiou@10.8.61.85>
2018-01-16 14:13:35 -0500

    Initial project created.

---
+++ src/java_prg_06_08_temperature_class/JAVA_PRG_06_08_Temperature_Class.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package java_prg_06_08_temperature_class;
+
+/**
+ *
+ * @author aeiou
+ */
+public class JAVA_PRG_06_08_Temperature_Class {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
